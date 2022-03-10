d097a611d850b6feaf7ad383075d3e76927e5c0e
Zane Birkett <axelbirkett@gmail.com>
2022-03-10 10:40:24 -0400

    Router & Pages Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import Home from "./components/pages/home/Home";
+import Create from "./components/pages/create/Create";
+import Recipe from "./components/pages/recipe/Recipe";
+import Search from "./components/pages/search/Search";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/">
+            <Search />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/pages/home/Home.js
@@ -0,0 +1,7 @@
+import "./Home.css";
+
+const Home = () => {
+  return <div>HELLO</div>;
+};
+
+export default Home;
