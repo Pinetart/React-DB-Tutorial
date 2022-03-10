8054aebe7b2deeb9e71b5e88862d033c3e68ccb7
Zane Birkett <axelbirkett@gmail.com>
2022-03-10 09:41:43 -0400

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import About from "./components/pages/About";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,24 @@
+import { useFetch } from "../hooks/useFetch";
+import "./Home.css";
+
 const Home = () => {
+  const {
+    data: articles,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/articles");
   return (
-    <div>
+    <div className="home">
       <h2>Homepage</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem vel ut
-        nam molestiae molestias minima, aut natus, ullam assumenda sapiente
-        tempora voluptas, sint explicabo magnam quod incidunt placeat! Id, ab!
-      </p>
+      {isPending && <div>Loading...</div>}
+      {error && <div>Loading...</div>}
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id} className="card">
+            <h3>{article.title}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
     </div>
   );
 };
