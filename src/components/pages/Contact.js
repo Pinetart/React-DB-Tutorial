beb480f8c29e9fedec50f7a371699edc8afa9303
Zane Birkett <axelbirkett@gmail.com>
2022-03-10 10:14:22 -0400

    Query Parameters

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,7 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 const Contact = () => {
+  const queryString = useLocation().search;
+
+  const queryParams = new URLSearchParams(queryString);
+  const name = queryParams.get("name");
+  console.log(name);
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here</h2>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem vel ut
         nam molestiae molestias minima, aut natus, ullam assumenda sapiente
