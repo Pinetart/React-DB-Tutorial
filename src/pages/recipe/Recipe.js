67bcd32b0e89c5b48f0bbe66768773a226d18547
Zane Birkett <axelbirkett@gmail.com>
2022-03-10 11:57:11 -0400

    Recipe Details Template

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -11,13 +11,20 @@ const Recipe = () => {
     error,
   } = useFetch(`http://localhost:8000/recipes/${id}`);
   return (
-    <div>
-      {isPending && <p className="p">Loading...</p>}
+    <div className="recipe">
+      {isPending && <p className="pending">Loading...</p>}
       {error && <p className="error">{error}</p>}
       {recipe && (
-        <div>
-          <h1>{recipe.title}</h1>
-        </div>
+        <>
+          <h2 className="page-title">{recipe.title}</h2>
+          <p>Takes {recipe.cookingTime} to make.</p>
+          <ul>
+            {recipe.ingredients.map((ing) => (
+              <li key={ing}>{ing}</li>
+            ))}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
       )}
     </div>
   );
