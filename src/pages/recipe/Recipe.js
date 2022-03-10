c9c3d4d434c278f351c36b98312ae7b566932558
Zane Birkett <axelbirkett@gmail.com>
2022-03-10 11:45:00 -0400

    Fetching a Single Recipe

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,7 +1,26 @@
 import "./Recipe.css";
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 
 const Recipe = () => {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+
+  const {
+    data: recipe,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:8000/recipes/${id}`);
+  return (
+    <div>
+      {isPending && <p className="p">Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {recipe && (
+        <div>
+          <h1>{recipe.title}</h1>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Recipe;
