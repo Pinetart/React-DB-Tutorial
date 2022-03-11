cd92e5ec2593cb4f35977b7d4fc3b6c7fcab620a
Zane Birkett <axelbirkett@gmail.com>
2022-03-11 10:30:27 -0400

    Search Results Page

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,30 @@
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import RecipeList from "../../components/RecipeList";
+import { useState } from "react";
+
 import "./Search.css";
 
 const Search = () => {
-  return <div>Search</div>;
+  const queryString = useLocation().search;
+  const queryParams = new URLSearchParams(queryString);
+  const query = queryParams.get("q");
+
+  const url = `http://localhost:8000/recipes?q=${query}`;
+  const { error, isPending, data: recipes } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {recipes.length !== 0 ? (
+        <RecipeList recipes={recipes} />
+      ) : (
+        <p className="novalue">No Results</p>
+      )}
+    </div>
+  );
 };
 
 export default Search;
