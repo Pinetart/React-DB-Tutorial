91c0d58006f07c1a010b460bfe7d057ab3c13d0b
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-03-13 15:56:01 -0400

    Setting Up Firebase Auth

--- src/hooks/modifyBook.js
+++ src/hooks/modifyBook.js
@@ -10,3 +10,8 @@ export const createBook = (title) => {
 export const deleteBook = (id) => {
   return deleteDoc(doc(db, "books", id));
 };
+import React from "react";
+
+export function modifyBook() {
+  return <div>modifyBook</div>;
+}
