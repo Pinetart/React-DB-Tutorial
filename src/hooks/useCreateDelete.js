393d37b91fa8fa04ec896aa69355d3ec1049817c
Zane Birkett <Zane.birkett@houstydesigns.com>
2022-03-13 23:54:48 -0400

    Assigning Users to Books

--- src/hooks/useCreateDelete.js
+++ src/hooks/useCreateDelete.js
@@ -1,10 +1,13 @@
 import { collection, addDoc, doc, deleteDoc } from "firebase/firestore";
 import { db } from "../firebase/config";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export const useCreateDelete = () => {
+  const { user } = useAuthContext();
   const createBook = (title) => {
     return addDoc(collection(db, "books"), {
       title: title,
+      uid: user.uid,
     });
   };
 
