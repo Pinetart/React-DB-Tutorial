e3be281b95b6a7dbd9b834a55c776c020ba2d981
Zane Birkett <axelbirkett@gmail.com>
2022-03-15 09:14:16 -0400

    Creating a Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// Components
+import Navbar from "./components/Navbar";
+
+// Pages
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
@@ -7,6 +12,7 @@ function App() {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
