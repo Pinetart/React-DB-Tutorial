dad1f371ecdf89cfdb4243a59e0603c07623f996
Zane Birkett <axelbirkett@gmail.com>
2022-03-15 09:39:20 -0400

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmwhM7UvlFGv2EF_N5zgwSvSC9o5tjvio",
+  authDomain: "mymoney-14d56.firebaseapp.com",
+  projectId: "mymoney-14d56",
+  storageBucket: "mymoney-14d56.appspot.com",
+  messagingSenderId: "1000321034498",
+  appId: "1:1000321034498:web:3aacce793a4b23a22a7a1b",
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
