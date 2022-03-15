6a32947a611715288a87824b25fb1863e33569a0
Zane Birkett <axelbirkett@gmail.com>
2022-03-15 09:24:06 -0400

    Making the Login Form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,41 @@
-import React from "react";
+import { React, useState } from "react";
 import styles from "./Login.module.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, SetEmail] = useState("");
+  const [password, SetPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(email, password);
+    SetEmail("");
+    SetPassword("");
+  }
+
+  return (
+    <form className={styles["login-form"]} onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => {
+            SetEmail(e.target.value);
+          }}
+          value={email}
+        ></input>
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => {
+            SetPassword(e.target.value);
+          }}
+          value={password}
+        ></input>
+      </label>
+      <button className="btn">Login</button>
+    </form>
+  );
 }
