2dbeb45507bef9f577e783724de06de30c6a4006
Zane Birkett <axelbirkett@gmail.com>
2022-03-15 10:03:41 -0400

    Creating a Signup Hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,36 @@
+import { projectAuth } from "../firebase/config";
+import { useState } from "react";
+
+export function useSignup() {
+  const [error, setError] = useState("");
+  const [isPending, setIsPending] = useState(false);
+
+  const signup = async (email, password, displayName) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      // signup user
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res.user);
+
+      if (!res) {
+        throw new Error("Could not complete signup");
+      }
+
+      // add display name to user
+      await res.user.updateProfile({ displayName: displayName });
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, signup };
+}
