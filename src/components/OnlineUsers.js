b5cf1a27117fd5585430b6130cc4945572b1ad6e
Zane Birkett <axelbirkett@gmail.com>
2022-03-21 16:09:31 -0400

    Fetching Users

---
+++ src/components/OnlineUsers.js
@@ -0,0 +1,22 @@
+import { useCollection } from "../hooks/useCollection";
+import Avatar from "./Avatar";
+
+import "./OnlineUsers.css";
+
+export default function OnlineUsers() {
+  const { documents, error } = useCollection("users");
+
+  return (
+    <div className="user-list">
+      <h2>Online Users</h2>
+      {error && <div className="error">{error}</div>}
+      {documents &&
+        documents.map((user) => (
+          <div className="user-list-item" key={user.id}>
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
+        ))}
+    </div>
+  );
+}
