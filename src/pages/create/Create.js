1c7d133ad67938f6b0c0b4796cd5b5b0d1e52f8c
Zane Birkett <axelbirkett@gmail.com>
2022-03-22 09:28:17 -0400

    Assigning Users

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Select from "react-select";
+import { useCollection } from "../../hooks/useCollection";
 
 // styles
 import "./Create.css";
@@ -12,6 +13,9 @@ const categories = [
 ];
 
 export default function Create() {
+  const { documents } = useCollection("users");
+  const [users, setUsers] = useState([]);
+
   // form field values
   const [name, setName] = useState("");
   const [details, setDetails] = useState("");
@@ -19,10 +23,19 @@ export default function Create() {
   const [category, setCategory] = useState("");
   const [assignedUsers, setAssignedUsers] = useState([]);
 
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map((user) => {
+        return { value: user, label: user.displayName };
+      });
+      setUsers(options);
+    }
+  }, [documents]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(name, details, dueDate, category);
+    console.log(name, details, dueDate, category, assignedUsers);
   };
 
   return (
@@ -64,7 +77,11 @@ export default function Create() {
         </label>
         <label>
           <span>Assign to:</span>
-          {/* select here later */}
+          <Select
+            options={users}
+            onChange={(option) => setAssignedUsers(option)}
+            isMulti
+          />
         </label>
 
         <button className="btn">Add Project</button>
